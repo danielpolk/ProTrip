73b567663adc0efc74ad3c6505d52afd4bef69b3
Madi <madelyn.e.helton@gmail.com>
2018-11-17 11:44:34 -0600

    adding authentication

---
+++ js/authentication.js
@@ -0,0 +1,32 @@
+
+//Creating a new user
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+
+//Current user sign-in
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+
+});
+
+//This is a listener for if the user is logged in or not
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+  } else {
+    // No user is signed in.
+  }
+});
+
+//Signing a user out
+firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  // An error happened.
+});
