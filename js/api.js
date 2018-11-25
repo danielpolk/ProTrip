3a94605e05efdca86a1bcd83845f5ae54b48be94
Mark <mark.faraj86@gmail.com>
2018-11-25 15:00:42 -0600

    testing error catching when file not found

--- js/api.js
+++ js/api.js
@@ -29,23 +29,21 @@ console.log("we are live")
 function gasStationFinder(lon, lat, city_input) {
     // var nameStored = [];
     // var gasPriceStored = [];
-    var queryURL = "http://api.mygasfeed.com/stations/radius/" + lat + "/" + lon + "/3/reg/Price/bpxxw96ps2.json";
+    var queryURL = "http://api.mygasfeed.com/stations/radius/" + lat + "/" + lon + "/7/reg/Price/bpxxw96ps2.json";
     // console.log("station url: " + queryURL)
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-
+        console.log("gas response" + JSON.stringify(response));
         gasStationResponse(response, city_input);
-       
-        console.log("gas city input " + city_input);
     })
 };
 
 function gasStationResponse(response, city_input) {
     
 
-    for (var i = 0; i < (3); i++) {
+    for (var i = 0; i < (response.stations.length - 10); i++) {
         var gas_station_name = response.stations[i].station;
         var gas_price = response.stations[i].reg_price;
         var gas_address = response.stations[i].address
@@ -58,7 +56,7 @@ function gasStationResponse(response, city_input) {
             return letter.toUpperCase();
         });
         //exclude gas station name is unbranded and no gas price and city name other than user input
-        if (gas_station_name !== "Unbranded" && gas_price !== "N/A" && gas_city_name === city_input) {
+        if (gas_station_name !== "Unbranded" && gas_price !== "N/A" && gas_city_name !== city_input) {
             console.log("testing");
             // creating the div for the gas station
             // create the element
@@ -67,24 +65,31 @@ function gasStationResponse(response, city_input) {
             var gas_div_col = $("<div>").addClass("col s12 m6")
             var gas_div = $("<div>").addClass("card")
             var gas_div_image = $("<div>").addClass("card-image")
-            // var gas_main_img = $("<img>").attr("src", "nothing")
-            var gas_name_span = $("<span>").addClass("card-title").text(gas_station_name)
+            var gas_main_img = $("<img>").attr("src", "http://nj1015.com/files/2018/06/RS7210_490883015.jpg?w=980&q=75")
+            var gas_name_span = $("<h2>").addClass("card-title").text(gas_station_name);
+                var source = 'assets/images/' + gas_station_name + '.png"'
+                var test2 = '"gas_logo"'
+                var error1 = "style='background-image: url('assets/images/bucees.png');'"
+                var gas_logo = $('<div class=' + test2 + 'onerror=' +error1+'style="background-image: url(' + source + ')"></div>')
+            
+            // var source = 'assets/images/GasStationLogos/' + gas_station_name + '.png"'
+            // var test2 = '"gas_logo"'
+            // var gas_logo = $('<div class=' + test2 + ' style="background-image: url(' + source + ')"></div>')
+            
+            
             var gas_fav_btn = $("<a class='fav-btn btn-floating halfway-fab waves-effect waves-light red'><i class='material-icons'>favorite_border</i></a>")
             // var event_rating = $("<div class='btn-small rating-btn' style='background-color:#" + color_rating + "';>" + res_rating + "/5</div>")
             // console.log("this is the food rating " + event_rating)
             var gas_div_content = $("<div>").addClass("card-content")
+            var prices = $('<h5>').addClass('gas-price').text("$" + gas_price + "/gal");
             var line_break1 = $("<br>");
             var line_break2 = $("<br>");
             var line_break3 = $("<br>");
             var gas_address_span = $("<span>").addClass("left").text("Address: " + gas_address + " " + gas_city_name + ", " + gas_state + ", " + gas_zipcode);
-            gas_div_image.append(gas_name_span).append(gas_fav_btn);
-            gas_div_content.append(line_break1).append(line_break2).append(gas_address_span).append(line_break3);
+            gas_div_image.append(gas_main_img).append(gas_name_span).append(gas_logo).append(gas_fav_btn);
+            gas_div_content.append(prices).append(line_break2).append(gas_address_span).append(line_break3);
             gas_div.append(gas_div_image).append(gas_div_content);
             gas_div_col.append(gas_div);
-
-        
-
-    
         }
         // here push the text to the div using the id
         $("#gas_cards").append(gas_div_col);
@@ -100,9 +105,9 @@ function restaurantFinder() {
         e.preventDefault();
         // make sure that the input will be all lower case and trimmed
         city_input = $("#city_input").val().trim().toLowerCase();
-        // console.log("city input "+city_input)
+        // console.log("city input " + city_input)
         state_input = $("#state_input").val();
-        // console.log("state input "+String(state_input))
+        // console.log("state input " + String(state_input))
 
         //getting the city ID for Zomato API
         var queryURL = "https://developers.zomato.com/api/v2.1/cities?apikey=e54720b38895f113317f79aa68f4ca8e&q=" + city_input;
@@ -115,8 +120,8 @@ function restaurantFinder() {
                 // console.log(cityCheck)
                 var stateCheck = response.location_suggestions[index].state_name;
                 // console.log(String(stateCheck))
-                console.log("first statement "+stateCheck.toLowerCase().includes(state_input.toLowerCase()))
-                // console.log("second statement "+(state_input == stateCheck))
+                console.log("first statement " + stateCheck.toLowerCase().includes(state_input.toLowerCase()))
+                // console.log("second statement " + (state_input == stateCheck))
                 //    var helo= hello.toLowerCase();
                 // var hithere=cityCheck.includes(city_input)
                 if (cityCheck.includes(city_input) && stateCheck.toLowerCase().includes(state_input.toLowerCase())) {
@@ -151,7 +156,7 @@ restaurantFinder();
 
 
 function eventFinder(city_input) {
-    var city_name_nospace = city_input.split(' ').join('+');
+    var city_name_nospace = city_input.split(' ').join(' + ');
     var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + city_name_nospace + "&apikey=hhGX8q6JtGAAl35uFcsEeWLTAuCdjSVc&size=8";
     // console.log("Event url: " + queryURL)
     $.ajax({
@@ -183,9 +188,9 @@ function eventFinder(city_input) {
             var event_tickets = $("<a href='" + event_link + "' class='left'>Buy Tickets</a>")
             var line_break1 = $("<br>");
             var line_break2 = $("<br>");
-            var event_date_span = $("<span>").addClass("left").text("Show Date: "+event_date +" at "+event_time);
+            var event_date_span = $("<span>").addClass("left").text("Show Date: " + event_date + " at " + event_time);
             var line_break3 = $("<br>");
-            var event_address_span = $("<span>").addClass("left").text("Event Address: " + event_venue +" at "+event_venue_name);
+            var event_address_span = $("<span>").addClass("left").text("Event Address: " + event_venue  + " at " + event_venue_name);
             event_div_image.append(event_main_img).append(event_name_span).append(event_fav_btn);
             event_div_content.append(event_tickets).append(line_break1).append(event_date_span).append(line_break2).append(event_address_span).append(line_break3);
             event_div.append(event_div_image).append(event_div_content);
@@ -205,16 +210,16 @@ function restaurantResponse(response) {
         var res_main_img = response.restaurants[i].restaurant.featured_image;
         console.log("image source " + res_main_img)
         var color_rating = response.restaurants[i].restaurant.user_rating.rating_color;
-        // console.log("this is color rating "+color_rating)
+        // console.log("this is color rating " + color_rating)
         var res_rating = response.restaurants[i].restaurant.user_rating.aggregate_rating;
-        // console.log("this is res rating "+res_rating)
+        // console.log("this is res rating " + res_rating)
         var menu_link = response.restaurants[i].restaurant.menu_url;
         var res_address = response.restaurants[i].restaurant.location.address;
 
         var food_div_col = $("<div>").addClass("col s12 m6")
         var food_div = $("<div>").addClass("card")
         var food_div_image = $("<div>").addClass("card-image")
-        if (res_main_img == ""){
+        if (res_main_img == "") {
             var food_main_img = $("<img>").attr("src", "assets/images/sub-res-image.jpeg")
         } else {
             var food_main_img = $("<img>").attr("src", res_main_img)
@@ -240,8 +245,8 @@ function restaurantResponse(response) {
     // here push the text to the div using the id
     $(document.body).on("click", ".fav-btn", function(){
     console.log($(this).parent())
-    console.log("test inner text " +$(this).parent().find(".material-icons").text())
-    // hello += $(this).parent().childNodes   .closest('tr').find('.sibbling').text()
+    console.log("test inner text "  + $(this).parent().find(".material-icons").text())
+    // hello  + = $(this).parent().childNodes   .closest('tr').find('.sibbling').text()
     
     $(this).parent().find(".fav-btn").removeClass("fav-btn").addClass("rmv-btn")
     $(this).parent().find(".material-icons").text("delete");
@@ -263,13 +268,13 @@ function restaurantResponse(response) {
     $("#fav_cards").append(food_div_col);
 
 
-       console.log("test user id "+ childSnapshot.val().food_div)
+       console.log("test user id " +  childSnapshot.val().food_div)
     
        
 });
 }
 
-// database.ref().orderByChild("dateAdded")
+
 
 
 
